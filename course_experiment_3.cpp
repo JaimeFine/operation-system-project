a84f46d9f6ae9a7e853c52ccb440aef6c7e82148
JaimeFine <jaimefine6@gmail.com>
2026-05-20 18:35:02 +0800

    Update

---
+++ course_experiment_3.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <string>
+#include <chrono>
+
+class CountingSemaphore {
+private:
+    std::mutex mtx;
+    std::condition_variable cv;
+    int count;
+
+public:
+    explicit CountingSemaphore(int initial_count = 1) : count(initial_count) {}
+
+    void acquire() {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [this]() {
+            return count > 0;
+        });
+        --count;
+    }
+
+    void release() {
+        std::unique_lock<std::mutex> lock(mtx);
+        ++count;
+        cv.notidy_one();
+    }
+};
+
+struct ThreadParam {
+    int id;
+    char type;
+    unsigned int delay;
+    unsigned int duration;
+};
+
+// Global synchronization primitives
+std::mutex g_mtx;
+CountingSemaphore g_writeSem(1);
+int g_nReaders = 0;
+
+// Thread-safe console output function
+void print_msg(const std::string& msg) {
+    static std::mutex count_mtx;
+    std::lock_guard<std::mutex> lock(cout_mtx);
+    std::cout << msg << std::endl;
+}
+
+void ReaderProc(ThreadParam p) {
+    // Reader request
+    print_msg("Reader thread " + std::to_string(p.id) + " sends R require...");
+
+    std::this_thread::sleep_for(std::chrono::seconds(p.delay));
+
+    {
+        std::lock_guard<std::mutex> lock(g_mtx);
+        g_nReader++;
+        if (g_nReaders == 1) {
+            g_writeSem.acquire();
+        }
+    }
+
+    print_msg("Reader thread " + std::to_string(p.id) + " begins to read.");
+    std::this_thread::sleep_for(std::chrono::seconds(p.duration));
+    print_msg("Reader thread " + std::to_string(p.id) + " finished reading.");
+
+    {
+        std::lock_guard<std::mutex> lock(g_mtx);
+        g_nReaders--;
+        if (g_nReaders == 0) {
+            g_writeSem.release();   // Wake up
+        }
+    }
+}
+
+void WriteProc(ThreadParam p) {
+    // Writer request
+    print_msg("Writer thread " + std::to_string(p.id) + " sends W require...");
+
+    std::this_thread::sleep_for(std::chrono::seconds(p.delay));
+
+    g_writeSem.acquire();
+
+    print_msg("Writer thread " + std::to_string(p.id) + " begins to write.");
+    std::this_thread::sleep_for(std::chrono::seconds(p.duration));
+    print_msg("Writer thread " + std::to_string(p.id) + "finished writing.");
+
+    g_writeSem.release();
+}
+
+int main() {
+    std::ifstream infile("test1.txt");
+    if (!infile.is_open()) {
+        std::cerr << "Error opening test1.txt" << std::endl;
+        return 1;
+    }
+
+    std::vector<std::thread> threads;
+    ThreadParam tempParam;
+
+    print_msg("Reader Priority:");
+
+    while (
+        infile >> tempParam.id >> tempParam.type >>
+        tempParam.delay >> tempParam.duration
+    ) {
+        if (tempParam.type == 'R') {
+            threads.emplace_back(ReaderProc, tempParam);
+        } else {
+            threads.emplace_back(WriterProc, tempParam);
+        }
+    }
+    infile.close();
+
+    // Join the threads
+    for (auto& t : threads) {
+        if (t.joinable()) {
+            t.join();
+        }
+    }
+
+    print_msg("All reader and writer have been finished operating");
+
+    return 0;
+}
